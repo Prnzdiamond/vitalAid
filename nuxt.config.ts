79e241556033cc5b51467473ddb9e8e05d28d4c3
Prnzdiamond <oseahumenagboifoh@gmail.com>
2025-04-19 15:25:19 +0100

    hmmmm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   modules: ['@pinia/nuxt','pinia-plugin-persistedstate/nuxt'],
+
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
+
   plugins: [
     "~/plugins/init-echo.client.js",
     '~/plugins/echo.client'
-    
   ],
+
   runtimeConfig: {
     public: {
-      apiBase: "https://vitalaid.duckdns.org/api",
+      apiBase: process.env.NODE_ENV === 'development' 
+        ? "http://localhost:8000/api" 
+        : "https://vitalaid.duckdns.org/api",
+
       reverbAppKey: "fwz3lcuedhtxbptxrbjx",
-      reverbHost: "vitalaid.duckdns.org",
+
+      reverbHost: process.env.NODE_ENV === 'development'
+        ? "localhost" 
+        : "vitalaid.duckdns.org",
+
       reverbPort: 8080,
     },
   },
--- plugins/init-echo.client.js
+++ plugins/init-echo.client.js
@@ -5,7 +5,7 @@ export default defineNuxtPlugin(() => {
     if (process.client) {
         const token = localStorage.getItem("token");
         if (token) {
-            initEcho(token); // Initialize Echo with the token
+            initEcho(token); 
         }
     }
 });
